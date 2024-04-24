244837b67a398db7aa2bfba12f04a220a3e9af0f
Cmte-Kirk <kirk.java@gmail.com>
2024-04-24 00:16:09 -0300

    Finalizado o curso Django: persistência de dados e Admin

--- galeria/admin.py
+++ galeria/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from galeria.models import Fotografia
+
+class ListandoFotografias(admin.ModelAdmin):
+    list_display = ("id", "nome", "legenda", "data_fotografia", "publicada")
+    ordering = ("id", "nome", "legenda", "data_fotografia", "publicada")
+    list_display_links = ("id", "nome")
+    search_fields = ("nome",)
+    list_filter = ("categoria",)
+    list_editable = ("publicada",)
+    list_per_page = 10
+
+
 # Register your models here.
+admin.site.register(Fotografia, ListandoFotografias)
